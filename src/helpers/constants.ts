00e700dd56b15f0369db62c52da9f8bd5ad560ae
Gabriel Nicolás González <gabrielnicolasgonzalez@gmail.com>
2022-03-15 09:47:42 -0300

    changed addresses for consumming honeyswap on gnosis chain

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -10,12 +10,10 @@ export let ONE_BD = BigDecimal.fromString('1')
 
 export let EMPTY_RESERVES_RESULT = new UniswapV2Pair__getReservesResult(ZERO_BI, ZERO_BI, ZERO_BI)
 
-export let UNISWAP_FACTORY = Address.fromString('0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f')
+export let UNISWAP_FACTORY = Address.fromString('0xA818b4F111Ccac7AA31D0BCc0806d64F2E0737D7')
 
 export let WETH_ADDRESS = new Map<string, Address>()
-WETH_ADDRESS.set('rinkeby', Address.fromString('0xc778417E063141139Fce010982780140Aa0cD5Ab'))
-WETH_ADDRESS.set('mainnet', Address.fromString('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'))
+WETH_ADDRESS.set('xdai', Address.fromString('0x6a023ccd1ff6f2045c3309768ead9e68f978f6e1'))
 
 export let STABLECOIN_ADDRESS = new Map<string, Address>()
-STABLECOIN_ADDRESS.set('rinkeby', Address.fromString('0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea'))
-STABLECOIN_ADDRESS.set('mainnet', Address.fromString('0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48')) // USDC
+STABLECOIN_ADDRESS.set('xdai', Address.fromString('0xe91d153e0b41518a2ce8dd3d7944fa863463a97d'))
